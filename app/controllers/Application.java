ec090a57e1cd30c8ceaadcb8346efff54c0a7e55
awsp <anthonyspwu@gmail.com>
2015-06-26 16:09:36 -1000

    init commit

---
+++ app/controllers/Application.java
@@ -0,0 +1,16 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+import play.twirl.api.Content;
+import views.html.*;
+
+
+public class Application extends Controller {
+
+    public Result index() {
+	    Content html = (Content) index.render("ddd");
+	    return ok(html);
+    }
+
+}
